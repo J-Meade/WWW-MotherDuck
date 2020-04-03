b8fe803a6d4bffc0ccb0257f5f76c1322ad622aa
Kyle <iamkylekellysemailaddress@gmail.com>
2020-04-03 12:12:29 +1100

    Reverted player controller back to Xbox controller; Keyboard controller package included in 'FPSController' folder

--- Assets/3rdPartyAssets/FPSControllerV3/PlayerController.cs
+++ Assets/3rdPartyAssets/FPSControllerV3/PlayerController.cs
@@ -100,28 +100,16 @@ public class PlayerController : MonoBehaviour {
 		// calculate how fast it should be moving
 		//Vector3 targetVelocity = new Vector3(CheckMoveDirection(XCI.GetAxis(XboxAxis.LeftStickX,controllerNumber),transform.right) * strafeSpeed, 0,CheckMoveDirection(XCI.GetAxis(XboxAxis.LeftStickY,controllerNumber),transform.forward)* forwardAndBackSpeed);
 		Vector3 targetVelocity = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX,controllerNumber) * strafeSpeed, 0, XCI.GetAxis(XboxAxis.LeftStickY,controllerNumber) * forwardAndBackSpeed);
-
-        /////
-        ///// Hacked in keyboard movement for both players (at the same time)
-        /////
-        Vector3 targetVelocityHack = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
-        /////
-        ///// Hacked in keyboard movement for both players (at the same time) --> + targetVelocityHack
-        /////
-        targetVelocity = transform.TransformDirection(targetVelocity + targetVelocityHack);
+		targetVelocity = transform.TransformDirection(targetVelocity);
 		
 		// apply a force that attempts to reach our target velocity
 		Vector3 velocity = GetComponent<Rigidbody>().velocity;
 		Vector3 velocityChange = (targetVelocity - velocity);
 		velocityChange.y = 0;
 		GetComponent<Rigidbody>().AddForce(velocityChange, ForceMode.VelocityChange);
-
-        // jump
-
-        /////
-        ///// Hacked in keyboard movement for both players (at the same time) --> ||Input.GetKeyDown(KeyCode.Space))
-        /////
-        if (canJump && isGrounded && (XCI.GetButton(XboxButton.A,controllerNumber)||Input.GetKeyDown(KeyCode.Space))) {
+		
+		// jump
+		if (canJump && isGrounded && XCI.GetButton(XboxButton.A,controllerNumber)) {
 			GetComponent<Rigidbody>().velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
 			isGrounded = false;
 		}
@@ -172,28 +160,16 @@ public class PlayerController : MonoBehaviour {
 
 
 			rotationY +=  XCI.GetAxis(XboxAxis.RightStickY,controllerNumber) * mouseSensitivityY;
-
-            /////
-            ///// Hacked in mouse movement for both players (at the same time)
-            /////
-            rotationY += Input.GetAxis("Mouse Y") * mouseSensitivityY;
-
-
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 
 			transform.localEulerAngles = new Vector3(0, rotationX, 0);
 			head.localEulerAngles = new Vector3(-rotationY,0,0);
-
-            /////
-            ///// Hacked in mouse movement for both players (at the same time)
-            /////
-            transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivityX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
+		}
+		else if (axes == RotationAxes.MouseX)
 		{
 			transform.Rotate(0, XCI.GetAxis(XboxAxis.RightStickX,controllerNumber) * mouseSensitivityX , 0);
-        }
-        else
+		}
+		else
 		{
 			rotationY +=  + XCI.GetAxis(XboxAxis.RightStickX,controllerNumber) * mouseSensitivityY  /3;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
